464a67e2a47a855db99105c17fb788bc105f06dc
Raoni Demarchi <raonidemarchi@gmail.com>
2018-06-28 21:13:59 -0300

    finalizado header

--- init.js
+++ init.js
@@ -15,8 +15,4 @@ async function getAllUsers() {
 	console.log(await ajax('https://api.mercadolibre.com/sites/'));
 }
 
-async function getUser(id = 0) {
-	console.log(await ajax('http://devfluig.iv2.com.br/api/public/2.0/users/findById/' + id));
-}
-
 getAllUsers();
