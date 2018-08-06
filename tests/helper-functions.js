40fb6e1c76f99cad4b437273b3fef135dbfa6bcc
Raoni Demarchi <raoni.demarchi@iv2.com.br>
2018-08-06 15:25:09 -0300

    adiciona testes unitários das funções helpers

---
+++ tests/helper-functions.js
@@ -0,0 +1,15 @@
+'use strict'
+require('../js/helper.js');
+QUnit.module('Helper functions');
+
+QUnit.test('format money test', (assert) => {
+    let result = 29990.20.formatMoney(2, ',', '.');
+
+    assert.ok(result === '29.990,20', 'Outputs ' + result + ', expects 29.990,20');
+});
+
+QUnit.test('capitalize text test', (assert) => {
+    let result = 'hello meli'.capitalize();
+
+    assert.ok(result === 'Hello meli', 'Outputs ' + result + ', expects Hello meli');
+});
